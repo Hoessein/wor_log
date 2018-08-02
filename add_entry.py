d9d17d1c663cbc45ec7ce3113190f0a2c88bc94c
Hoessein Abdullahi <habdullahi@saa.nl>
2018-08-02 11:17:42 +0200

    Initial commit

---
+++ add_entry.py
@@ -0,0 +1,53 @@
+import datetime
+import os
+import csv
+
+class AddEntry(object):
+
+    def __init__(self):
+        pass
+
+    def task_date(self):
+        while True:
+            try:
+                task_date = input("What is the date of the task? Please use DD/MM/YYYY")
+                datetime.datetime.strptime(task_date, '%d/%m/%Y')
+                return task_date
+            except ValueError:
+                print("Please enter a date in the DD/MM/YYYY format")
+
+    def task_title(self):
+        task_title = input("What is the title of the task? ")
+        return task_title
+
+    def time_spent(self):
+        while True:
+            try:
+                time_spent = int(input("How many minutes did you work on it? "))
+                return time_spent
+            except ValueError:
+                print("Please enter the amount of minutes in numbers")
+
+    def notes(self):
+        notes = input("Do you have any additional notes? ")
+        return notes
+
+
+    def write_to_csv(self):
+        file_exists = os.path.isfile('test.csv')
+
+        with open('test.csv', 'a') as new_file:
+            fieldnames = ['TaskDate', 'TaskTitle', 'Minutes', 'Notes']
+
+            csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
+
+            if not file_exists:
+                csv_writer.writeheader()
+
+            csv_writer.writerow({'TaskDate': add_entry.task_date(), 'TaskTitle': add_entry.task_title(),
+                                 'Minutes': add_entry.time_spent(), 'Notes': add_entry.notes()})
+
+        print("The entry has been added. Press enter to return to the menu! ")
+
+add_entry = AddEntry()
+add_entry.write_to_csv()
---
+++ search_entry.py
@@ -0,0 +1,48 @@
+import csv
+import re
+
+class SearchEntry(object):
+
+    def __init__(self):
+        self.csv_file = csv.DictReader(open('test.csv', "rt"), delimiter="\t")
+
+    def find_by_date(self):
+        find_by_date  = input("enter date")
+        for row in self.csv_file:
+            if row['TaskDate'] == find_by_date:
+                print(row, "heyyy")
+
+    def find_by_time_spent(self):
+        find_by_time_spent = input("enter time spent")
+        for row in self.csv_file:
+            if row['Minutes'] == find_by_time_spent:
+                print(row)
+
+    def find_by_exact_search(self):
+        find_by_exact_search = input("Enter your exact search")
+        for row in self.csv_file:
+            if row['TaskTitle'] or row['Notes'] == find_by_exact_search:
+                print(row)
+
+    def find_by_pattern(self):
+
+        find_by_pattern = input(r"Enter your pattern")
+        pattern = re.compile(find_by_pattern)
+
+        for row in self.csv_file:
+            print(re.search(pattern, row['TaskDate']).group())
+            print(row)
+
+            # print(row['TaskTitle'])
+            # print(pattern)
+            # match = pattern.match(row['TaskTitle']).groupdict()
+            # print(match)
+            # # for row in self.csv_file:
+            # #     for r in pattern.finditer(row['TaskTitle']):
+            # #         print(r)
+
+
+search_entry = SearchEntry()
+search_entry.find_by_pattern()
+
+# some = re.findall(find_by_pattern, row['TaskTitle'])
