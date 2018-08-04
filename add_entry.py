963f7ee4be963834c2033b6a0916b3f6765eb619
Hoessein Abdullahi <habdullahi@saa.nl>
2018-08-04 18:20:18 +0200

    some doctypes, i should do these immediately.

--- add_entry.py
+++ add_entry.py
@@ -8,6 +8,7 @@ class AddEntry(object):
         pass
 
     def task_date(self):
+        '''Takes an input from the user and makes sure it is in the write format'''
         while True:
             try:
                 task_date = input("What is the date of the task? Please use DD/MM/YYYY")
@@ -16,11 +17,14 @@ class AddEntry(object):
             except ValueError:
                 print("Please enter a date in the DD/MM/YYYY format")
 
+
     def task_title(self):
+        '''Takes a task title from the user'''
         task_title = input("What is the title of the task? ")
         return task_title
 
     def time_spent(self):
+        '''Takes an input from the user, the input can only be numbers'''
         while True:
             try:
                 time_spent = int(input("How many minutes did you work on it? "))
@@ -29,11 +33,13 @@ class AddEntry(object):
                 print("Please enter the amount of minutes in numbers")
 
     def notes(self):
+        '''Takes additional notes from the user as input'''
         notes = input("Do you have any additional notes? ")
         return notes
 
 
     def write_to_csv(self):
+        '''writes the data entered by the user to a csv_file'''
         file_exists = os.path.isfile('test.csv')
 
         with open('test.csv', 'a') as new_file:
@@ -41,6 +47,7 @@ class AddEntry(object):
 
             csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter='\t')
 
+            #only if the file does not exist, csv_writer will write the headers
             if not file_exists:
                 csv_writer.writeheader()
 
--- search_entry.py
+++ search_entry.py
@@ -8,6 +8,9 @@ class SearchEntry(object):
         self.csv_file = csv.DictReader(open('test.csv', "rt"), delimiter="\t")
 
     def find_by_date(self):
+        '''The user is asked to input a date.
+           The user is then shown a corresponding work log entry'''
+
         find_by_date  = input("enter date")
         for row in self.csv_file:
             if row['TaskDate'] == find_by_date:
@@ -18,6 +21,10 @@ class SearchEntry(object):
                       )
 
     def find_by_time_spent(self):
+        '''The user is asked to input a number of minutes.
+            The user is then showed a work log entry corresponding
+            the entered minutes'''
+
         find_by_time_spent = input("enter time spent")
         for row in self.csv_file:
             if row['Minutes'] == find_by_time_spent:
@@ -29,8 +36,12 @@ class SearchEntry(object):
                           )
 
     def find_by_exact_search(self):
+        '''The user is asked to input a search criteria
+            and prints out corresponding work log entries'''
+
         find_by_exact_search = input("Enter your exact search")
         for row in self.csv_file:
+            #The exact search is based on the columns: 'TaskTitle' and 'Notes'
             if row['TaskTitle'] or row['Notes'] == find_by_exact_search:
                 print("Task date: " + row['TaskDate'] + "\n"
                       "Task title: " + row['TaskTitle'] + "\n"
@@ -39,7 +50,7 @@ class SearchEntry(object):
                       )
 
     def find_by_pattern(self):
-
+        '''A regex input is expected, this method check if there is a match and prints it out'''
         find_by_pattern = input(r"Enter your pattern")
 
         names_file = open("test.csv")
@@ -50,9 +61,10 @@ class SearchEntry(object):
         for row in self.csv_file:
             for x in set(pattern):
 
-                if x in row.values():
+                if x in row['TaskTitle']:
                     print("Task date: " + row['TaskDate'] + "\n"
                           "Task title: " + row['TaskTitle'] + "\n"
                           "Minutes: " + row['Minutes'] + "\n"
                           "Notes: " + row['Notes'] + "\n"
                           )
+
