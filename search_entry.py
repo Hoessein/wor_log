be7f685ebc03f32d714c992b142a67e5b37963b5
Hoessein Abdullahi <habdullahi@saa.nl>
2018-08-04 02:32:47 +0200

    regex search doing my head in

--- search_entry.py
+++ search_entry.py
@@ -1,5 +1,6 @@
 import csv
 import re
+from collections import OrderedDict
 
 class SearchEntry(object):
 
@@ -10,28 +11,73 @@ class SearchEntry(object):
         find_by_date  = input("enter date")
         for row in self.csv_file:
             if row['TaskDate'] == find_by_date:
-                print(row, "heyyy")
+                print("Task date: " + row['TaskDate'] + "\n"
+                      "Task title: " + row['TaskTitle'] + "\n"
+                      "Minutes: " + row['Minutes'] + "\n"
+                      "Notes: " + row['Notes'] + "\n"
+                      )
 
     def find_by_time_spent(self):
         find_by_time_spent = input("enter time spent")
         for row in self.csv_file:
             if row['Minutes'] == find_by_time_spent:
-                print(row)
+                if row['Minutes'] == find_by_time_spent:
+                    print("Task date: " + row['TaskDate'] + "\n"
+                          "Task title: " + row['TaskTitle'] + "\n"
+                          "Minutes: " + row['Minutes'] + "\n"
+                          "Notes: " + row['Notes'] + "\n"
+                          )
 
     def find_by_exact_search(self):
         find_by_exact_search = input("Enter your exact search")
         for row in self.csv_file:
             if row['TaskTitle'] or row['Notes'] == find_by_exact_search:
-                print(row)
+                print("Task date: " + row['TaskDate'] + "\n"
+                      "Task title: " + row['TaskTitle'] + "\n"
+                      "Minutes: " + row['Minutes'] + "\n"
+                      "Notes: " + row['Notes'] + "\n"
+                      )
 
     def find_by_pattern(self):
 
         find_by_pattern = input(r"Enter your pattern")
-        pattern = re.compile(find_by_pattern)
+
+        names_file = open("test.csv")
+        data = names_file.read()
+
+        pattern = re.findall(find_by_pattern, data)
+
+        print('------------------')
+        print('Inside of Pattern: \n', pattern)
+        print('------------------')
 
         for row in self.csv_file:
-            print(re.search(pattern, row['TaskDate']).group())
-            print(row)
+            for x in pattern:
+                if x in row.values():
+                    print(row)
+
+        # for row in data:
+        #     print(row)
+        #
+        # for row in self.csv_file:
+        #     for x in pik:
+        #         if x == row['TaskDate']:
+        #             pik2.append(row)
+        # print(len(pik2))
+
+
+
+            # for field in row:
+            #     # if field ==
+            #     #     print(row)
+
+
+                # for x in column:
+                #     print(x)
+                # "Task title: ", re.search(pattern, row['TaskTitle']).group() + "\n"
+                  # "minutes: ", re.search(pattern, row['Minutes']).group() + "\n"
+                  # "Notes: ", re.search(pattern, row['Notes']).group() + "\n"
+
 
             # print(row['TaskTitle'])
             # print(pattern)
--- work_log.py
+++ work_log.py
@@ -3,24 +3,50 @@ import os.path
 import datetime
 from add_entry import AddEntry
 from search_entry import SearchEntry
+import sys
 
+
+def instructions():
+    while True:
+        print("WORK LOG")
+        some = input("What would you like to do?\n"
+                     "a) Add a new entry\n"
+                     "b) Search in existing entries\n"
+                     "c) Quit program\n")
+        if some == 'a':
+            add_entry_prompt()
+        elif some == 'b':
+            search_entry_prompt()
+        elif some == 'c':
+            break
 
 def add_entry_prompt():
-    print("WORK LOG")
-    some = input("What would you like to do?\n"
-        "a) Add a new entry\n"
-        "b) Search in existing entries\n"
-        "c) Quit program\n")
+            add_entry.write_to_csv()
+            back_to_menu()
 
-    if some == 'a':
-        addd_entry = AddEntry()
-        addd_entry.task_date()
-        addd_entry.task_title()
-        addd_entry.time_spent()
-        addd_entry.notes()
-        addd_entry.write_to_csv()
-        print("The entry has been added. Press enter to return to the menu! ")
+def back_to_menu():
+        back_to_worklog = input("The entry has been added. Press any key to return to the menu! ")
+        if back_to_worklog == "":
+            instructions()
 
+
+def search_entry_prompt():
+            #clear
+            search_by = input("Do you want to search by: \n"
+            "a) Exact date\n"
+            "b) Exact search\n" 
+            "c) Regex pattern\n"
+            "d) Time spent\n"
+            "e) return to menu")
+
+            if search_by == 'a':
+                search_entry.find_by_date()
+            elif search_by == 'b':
+                search_entry.find_by_exact_search()
+            elif search_by == 'c':
+                search_entry.find_by_pattern()
+            elif search_by == 'd':
+                search_entry.find_by_time_spent()
 
     # gebruiker moet een invoer kunnen doen en deze moet opgeslagen worden
     # As a user of the script, if I choose to enter a new work log,
@@ -74,7 +100,8 @@ def add_entry_prompt():
     # #
     #
 
-
+search_entry = SearchEntry()
+add_entry = AddEntry()
 
 if __name__ == '__main__':
-    add_entry_prompt()
+    instructions()
